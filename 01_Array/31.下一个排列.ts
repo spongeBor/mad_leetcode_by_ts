cac30b412f01eba1f1ef984bf686b6e38691343c
spongeB <43675626+spongeBor@users.noreply.github.com>
2023-04-20 00:27:19 +0800

    feat(array): add 31

---
+++ 01_Array/31.下一个排列.ts
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=typescript
+ *
+ * [31] 下一个排列
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+// 算法描述：
+//1、需要将一个左边的「较小数」与一个右边的「较大数」交换，以能够让当前排列变大，从而得到下一个排列。
+//2、同时我们要让这个「较小数」尽量靠右，而「较大数」尽可能小。当交换完成后，「较大数」右边的数需要按照升序重新排列。这样可以在保证新排列大于原来排列的情况下，使变大的幅度尽可能小。
+
+function nextPermutation(nums: number[]): void {
+  let i = nums.length - 2;
+  while (i >= 0 && nums[i] >= nums[i + 1]) {
+    i--;
+  }
+  if (i >= 0) {
+    let j = nums.length - 1;
+    while (j >= 0 && nums[i] >= nums[j]) {
+      j--;
+    }
+    [nums[i], nums[j]] = [nums[j], nums[i]];
+  }
+  const reverse = (nums: number[], start: number) => {
+    let left = start,
+      right = nums.length - 1;
+    while (left < right) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left++;
+      right--;
+    }
+  };
+  reverse(nums, i + 1);
+}
+// @lc code=end
