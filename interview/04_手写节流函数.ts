da553743901c3afa4f2dc2df27d84112617631b7
spongeB <43675626+spongeBor@users.noreply.github.com>
2023-04-22 22:58:41 +0800

    feat(interview): add 04

---
+++ interview/04_手写节流函数.ts
@@ -0,0 +1,12 @@
+function throttle(fn: Function, delay = 200) {
+  let flag = true;
+  return function () {
+    if (!flag) return;
+    flag = false;
+    const args = arguments;
+    setTimeout(() => {
+      fn.apply(this, args);
+      flag = true;
+    }, delay);
+  };
+}
