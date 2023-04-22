4a2d8dfd59070032860f4b487a8ecb4619d71fb2
spongeB <43675626+spongeBor@users.noreply.github.com>
2023-04-22 10:47:27 +0800

    feat(abobe): add 3

---
+++ Company/Abobe/3.无重复字符的最长子串.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=typescript
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+// 滑动窗口
+function lengthOfLongestSubstring(s: string): number {
+  // 哈希集合，记录每个字符是否出现过
+  const occ = new Set();
+  const n = s.length;
+  // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+  let rk = -1,
+    ans = 0;
+  for (let i = 0; i < n; i++) {
+    if (i !== 0) {
+      // 左指针向右移动一格，移除一个字符
+      occ.delete(s[i - 1]);
+    }
+    while (rk + 1 < n && !occ.has(s[rk + 1])) {
+      // 不断地移动右指针
+      occ.add(s[rk + 1]);
+      rk++;
+    }
+
+    // 第 i 到 rk 个字符是一个极长的无重复字符子串
+    ans = Math.max(ans, rk - i + 1);
+  }
+  return ans;
+}
+// @lc code=end
